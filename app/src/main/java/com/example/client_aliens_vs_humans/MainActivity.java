dc43cd069bf9d2526eeedfdf4d0141b156ff4e38
nicolaspenagos <nicolas.penagosm98@gmail.com>
2020-10-21 01:24:07 -0500

    Arrows has been implemented.

--- app/src/main/java/com/example/client_aliens_vs_humans/MainActivity.java
+++ app/src/main/java/com/example/client_aliens_vs_humans/MainActivity.java
@@ -11,15 +11,24 @@ package com.example.client_aliens_vs_humans;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.client_aliens_vs_humans.events.OnMessageListener;
+import com.example.client_aliens_vs_humans.tcpmodel.Direction;
+import com.google.gson.Gson;
 
-public class MainActivity extends AppCompatActivity implements OnMessageListener {
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity implements OnMessageListener, View.OnClickListener{
 
     private TCPConnection tcp;
-    private Button button;
+    private Button upButton;
+    private Button downButton;
+    private Button rightBurron;
+    private Button leftButton;
+    private Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,24 +36,73 @@ public class MainActivity extends AppCompatActivity implements OnMessageListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
+        gson = new Gson();
+
+        upButton = findViewById(R.id.upButton);
+        downButton = findViewById(R.id.downButton);
+        rightBurron = findViewById(R.id.rightButton);
+        leftButton = findViewById(R.id.leftButton);
 
         tcp = TCPConnection.getInstance();
         tcp.setObserver(this);
         tcp.start();
 
-        button.setOnClickListener(
-
-                (v)->{
-                    tcp.sendMessage("Hola desde android");
-                }
-
-        );
+        upButton.setOnClickListener(this);
+        downButton.setOnClickListener(this);
+        rightBurron.setOnClickListener(this);
+        leftButton.setOnClickListener(this);
 
     }
 
     @Override
     public void onMessage(String msg) {
         runOnUiThread(()->Toast.makeText(this, msg, Toast.LENGTH_SHORT).show());
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Direction direction;
+        String json;
+        char dir;
+
+        switch (view.getId()){
+
+            case R.id.upButton:
+
+                direction = new Direction(UUID.randomUUID().toString(), Direction.UP, "Is the direction of the player on the matrix");
+                json = gson.toJson(direction);
+
+                tcp.sendMessage(json);
+
+                break;
+
+            case R.id.downButton:
+
+                direction = new Direction(UUID.randomUUID().toString(), Direction.DOWN, "Is the direction of the player on the matrix");
+                json = gson.toJson(direction);
+
+                tcp.sendMessage(json);
+
+                break;
+
+            case R.id.rightButton:
+
+                direction = new Direction(UUID.randomUUID().toString(), Direction.RIGHT, "Is the direction of the player on the matrix");
+                json = gson.toJson(direction);
+
+                tcp.sendMessage(json);
+
+                break;
+
+            case R.id.leftButton:
+
+                direction = new Direction(UUID.randomUUID().toString(), Direction.LEFT, "Is the direction of the player on the matrix");
+                json = gson.toJson(direction);
+
+                tcp.sendMessage(json);
+
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/client_aliens_vs_humans/tcpmodel/Generic.java
@@ -0,0 +1,15 @@
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * @author Nicolas Penagos Montoya
+ * nicolas.penagosm98@gmail.com
+ *
+ * @author Valentina Zapata Zapata
+ * valzapataz@gmail.com
+ **~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+package com.example.client_aliens_vs_humans.tcpmodel;
+
+public class Generic {
+
+    public String type;
+
+}
